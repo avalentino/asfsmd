5d0603420387d54d08a69292bb2897db1c78f53c
Antonio Valentino <Antonio.Valentino@esa.int>
2022-01-09 20:18:15 +0100

    Command line interface

--- asfsmd.py
+++ asfsmd.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
+# PYTHON_ARGCOMPLETE_OK
 
+"""ASF Sentinel-1 Metadata Download tool.
+
+Small Python tool (`asfsmd`) that allows to download XML files containing
+Sentinel-1 products metadata from the ASF archive.
+
+Sentinel-1 products are stored in the ASF arcive as ZIP files that are
+quite large because they comntain both the products annotations and the
+binary image data.
+
+The `asfsmd` tool is able to retrieve only the relatively samll annotation
+files (in XML format) without downloading the entire ZIP archive.
+"""
+
+import sys
+import netrc
 import fnmatch
 import logging
 import pathlib
 import zipfile
-import itertools
+import argparse
+import warnings
+
 from urllib.parse import urlparse
 
 import tqdm
@@ -13,20 +31,13 @@ import requests
 import asf_search as asf
 
 
-PRODUCTS = [
-    'S1A_IW_SLC__1SDV_20210502T171502_20210502T171529_037712_04733D_777C',
-    'S1A_IW_SLC__1SDV_20210502T171527_20210502T171554_037712_04733D_DE21',
-    'S1A_IW_SLC__1SDV_20210514T171503_20210514T171530_037887_0478B1_3294',
-    'S1A_IW_SLC__1SDV_20210514T171528_20210514T171555_037887_0478B1_2954',
-    'S1A_IW_SLC__1SDV_20210506T053504_20210506T053531_037763_0474F3_0892',
-    'S1A_IW_SLC__1SDV_20210506T053529_20210506T053556_037763_0474F3_EA2A',
-    'S1B_IW_SLC__1SDV_20210512T053432_20210512T053459_026867_0335B6_81DE',
-    'S1B_IW_SLC__1SDV_20210512T053457_20210512T053524_026867_0335B6_160F',
-]
-# PRODUCTS = list(itertools.chain(*PRODUCTS.values()))
-# print(PRODUCTS.values())
+__version__ = "1.0.0"
+
 
 _log = logging.getLogger(__name__)
+
+
+BLOACKSIZE=8*1024   # 8kbytes
 
 
 class HttpIOFile(httpio.SyncHTTPIOFile):
@@ -54,7 +65,8 @@ def query(products, auth=None):
     return results
 
 
-def download_annotations_core(urls, outdir='.', auth=None):
+def download_annotations_core(urls, outdir='.', auth=None,
+                              block_size=BLOACKSIZE):
     outdir = pathlib.Path(outdir)
 
     patterns = [
@@ -78,19 +90,24 @@ def download_annotations_core(urls, outdir='.', auth=None):
 
             _log.debug(f'download: {product_name}')
 
-            with HttpIOFile(url).open(session=session) as fd:
+            remote_file = HttpIOFile(url, block_size=block_size)
+            with remote_file.open(session=session) as fd:
                 _log.debug(f'{url} open')
                 with zipfile.ZipFile(fd) as zf:
-                    component_iter = tqdm.tqdm(zf.filelist, unit='files')
-                    for info in component_iter:
-                        component_iter.set_description(info.filename)
+                    components = []
+                    for info in zf.filelist:
                         for pattern in patterns:
                             if fnmatch.fnmatch(info.filename, pattern):
+                                components.append(info)
                                 break
-                        else:
-                            continue
-                        targetdir = outdir / pathlib.Path(info.filename).parent
-                        outfile = targetdir / pathlib.Path(info.filename).name
+
+                    component_iter = tqdm.tqdm(components, unit='files',
+                                               leave=False)    
+                    for info in component_iter:
+                        filename = pathlib.Path(info.filename)
+                        component_iter.set_description(filename.name)
+                        targetdir = outdir / filename.parent
+                        outfile = targetdir / filename.name
                         _log.debug(f'{targetdir = }')
                         _log.debug(f'{outfile = }')
                         targetdir.mkdir(exist_ok=True, parents=True)
@@ -105,25 +122,177 @@ def download_annotations_core(urls, outdir='.', auth=None):
 
 def download_annotations(products, outdir='.', auth=None):
     results = query(products, auth=auth)
-    assert len(results) == len(products)
+    if len(results) != len(products):
+        warnings.warn(
+            f"only {len(results)} of the {len(products)} requested products "
+            f"found on the remote server")
 
     urls = [item.properties['url'] for item in results]
 
     download_annotations_core(urls, outdir=outdir, auth=auth)
 
 
-def _get_auth():
-    import netrc
-    db = netrc.netrc()
-    user, _, pwd = db.authenticators('urs.earthdata.nasa.gov')
-    return user, pwd
+def _get_auth(*, user=None, pwd=None, hostname='urs.earthdata.nasa.gov'):
+    if user is not None and pwd is not None:
+        return user, pwd
+    elif user is None and pwd is None:
+        db = netrc.netrc()
+        user, _, pwd = db.authenticators(hostname)
+        return user, pwd    
+    else:
+        raise ValueError(
+            "Both username and password must be provided to authenticate.")
+
+
+# === Command Line Interface ==================================================
+
+
+try:
+    from os import EX_OK
+except ImportError:
+    EX_OK = 0
+EX_FAILURE = 1
+EX_INTERRUPT = 130
+
+LOGFMT = '%(asctime)s %(levelname)-8s -- %(message)s'
+
+
+def _autocomplete(parser):
+    try:
+        import argcomplete
+    except ImportError:
+        pass
+    else:
+        argcomplete.autocomplete(parser)
+
+
+def _set_logging_control_args(parser, default_loglevel='WARNING'):
+    """Setup command line options for logging control."""
+    loglevels = [logging.getLevelName(level) for level in range(10, 60, 10)]
+
+    parser.add_argument(
+        '--loglevel', default=default_loglevel, choices=loglevels,
+        help='logging level (default: %(default)s)')
+    parser.add_argument(
+        '-q', '--quiet', dest='loglevel', action='store_const',
+        const='ERROR',
+        help='suppress standard output messages, '
+             'only errors are printed to screen')
+    parser.add_argument(
+        '-v', '--verbose', dest='loglevel', action='store_const',
+        const='INFO', help='print verbose output messages')
+    parser.add_argument(
+        '-d', '--debug', dest='loglevel', action='store_const',
+        const='DEBUG', help='print debug messages')
+
+    return parser
+
+
+def _get_parser(subparsers=None):
+    """Instantiate the command line argument (sub-)parser."""
+    name = pathlib.Path(__file__).stem
+    synopsis = __doc__.splitlines()[0]
+    doc = __doc__
+
+    if subparsers is None:
+        parser = argparse.ArgumentParser(prog=name, description=doc)
+        parser.add_argument(
+            "--version", action="version", version='%(prog)s v' + __version__)
+    else:
+        parser = subparsers.add_parser(name, description=doc, help=synopsis)
+        # parser.set_defaults(func=info)
+
+    parser = _set_logging_control_args(parser)
+
+    # Command line options
+    parser.add_argument(
+        "-f", "--file-list", action="store_true",
+        help="read the list of products form file. "
+             "The file is expected to contain one product name per line.")
+    parser.add_argument(
+        "-o", "--outdir", default=".",
+        help="path of the output directory (default='%(default)s')")
+    
+    parser.add_argument(
+        "-u", "--username",
+        help="username for ASF authentication. "
+             "If not provided the tool attempts to retireve the "
+             "authentication parameters for the user's '.netrc' file looking "
+             "for the host 'urs.earthdata.nasa.gov'")
+    parser.add_argument(
+        "-p", "--password",
+        help="password for ASF authentication. "
+             "If not provided the tool attempts to retireve the "
+             "authentication parameters for the user's '.netrc' file looking "
+             "for the host 'urs.earthdata.nasa.gov'")
+    parser.add_argument(
+        "--block-size", type=int, default=BLOACKSIZE,
+        help="httpio block size in bytes (default: %(default)d)")
+
+    # Positional arguments
+    parser.add_argument(
+        "inputs", nargs="+", metavar="INPUT",
+        help="Sentinel-1 product name(s). "
+             "If the '-f' flag is set then the argument is interpreted as "
+             "the filename containing the list of products.")
+
+    if subparsers is None:
+        _autocomplete(parser)
+
+    return parser
+
+
+def _parse_args(args=None, namespace=None, parser=None):
+    """Parse command line arguments."""
+    if parser is None:
+        parser = _get_parser()
+
+    args = parser.parse_args(args, namespace)
+
+    # Common pre-processing of parsed arguments and consistency checks
+    # ...
+
+    return args
+
+
+def main(*argv):
+    """Main CLI interface."""
+    # setup logging
+    logging.basicConfig(format=LOGFMT, level=logging.INFO)  # stream=sys.stdout
+    logging.captureWarnings(True)
+
+    # parse cmd line arguments
+    args = _parse_args(argv if argv else None)
+    
+    # execute main tasks
+    exit_code = EX_OK
+    try:
+        _log.setLevel(args.loglevel)
+
+        products = []
+        if args.file_list:
+            for filename in args.inputs:
+                filename = pathlib.Path(filename)
+                products.extend(
+                    line for line in filename.read_text().splitlines() if line)
+        else:
+            products.extend(args.inputs)
+
+        auth = _get_auth(user=args.username, pwd=args.password)
+
+        download_annotations(products, outdir=args.outdir, auth=auth)
+    except Exception as exc:
+        _log.critical(
+            'unexpected exception caught: {!r} {}'.format(
+                type(exc).__name__, exc))
+        _log.debug('stacktrace:', exc_info=True)
+        exit_code = EX_FAILURE
+    except KeyboardInterrupt:
+        _log.warning('Keyboard interrupt received: exit the program')
+        exit_code = EX_INTERRUPT
+
+    return exit_code
 
 
 if __name__ == '__main__':
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s %(levelname)s: %(message)s'
-    )
-    logging.getLogger(__name__).setLevel(logging.DEBUG)
-
-    download_annotations(PRODUCTS, outdir='asf-out', auth=_get_auth())
+    sys.exit(main())
